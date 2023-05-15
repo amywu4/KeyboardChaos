842cb122da3bc70ede80d206a89aa45936fd9092
aw628 <aw628@cornell.edu>
2023-05-15 15:32:11 -0400

    Update mli files

--- src/state.mli
+++ src/state.mli
@@ -6,8 +6,9 @@
 
 type t
 (** The abstract type of values representing the game's state. Each state
-    contains information for the current points, level, prompt, rules, and
-    available rules for future levels.*)
+    contains information for the current points, level, prompt, current rules,
+    and available rules for future levels. Requires: there is no overlap between
+    the current rules and available rules.*)
 
 exception NoRules
 (** Raised when the game state has run out of available rules to give for the
@@ -17,7 +18,8 @@ val init_state : Text_shooting.t -> t
 (** [init_state a] is the initial state of the game when playing gamemode [a].
     In that state the player is currently playing level one, they have 0 points,
     and they currently have one randomly chosen prompt and the first rule in the
-    list of rules to follow. *)
+    list of rules to follow, with the rest of rules put into the state's list of
+    available rules.*)
 
 val current_points : t -> int
 (** [current_points s] is the amount of points the player has currently
@@ -42,6 +44,5 @@ val available_rules : t -> Text_shooting.rule list
 val next_level : int -> Text_shooting.t -> t -> t
 (** [next_level pts a s] modifies game state [s] by incrementing its points by
     pts, incrementing the level by 1, replacing the prompt with a new random
-    prompt from gamemode [a], and adding a new unique random rule from gamemmode
-    [a]. Requires: The new unique rule is not one that has already been used in
-    a previous level.*)
+    prompt from gamemode [a], and adding a new unique random rule based on state
+    [s]'s list of available rules.*)
--- src/text_shooting.mli
+++ src/text_shooting.mli
@@ -30,11 +30,11 @@ val from_json : Yojson.Basic.t -> t
     Requires: [j] is a valid JSON prompt representation. *)
 
 val get_prompts : t -> prompt list
-(** [get_prompts j] is a list of prompts in [t] *)
+(** [get_prompts t] is a list of prompts in [t] *)
 
 val random_prompt : prompt list -> prompt
-(** [random_prompt t] is a prompt randomly picked from the list of prompts
-    [t.prompts]. Raises [NoPrompts] if [t] has no prompts.*)
+(** [random_prompt lst] is a prompt randomly picked from the list of prompts
+    [lst]. Raises [NoPrompts] if [lst] has no prompts.*)
 
 val rule_name : rule -> string
 (** [rule_name r] is the name of a rule [r] *)
@@ -43,11 +43,11 @@ val rule_description : rule -> string
 (** [rule_description r] is the description of a rule [r] *)
 
 val get_rules : t -> rule list
-(** [get_rules_names j] is a list of rules that [j] has *)
+(** [get_rules_names t] is a list of rules that [t] has *)
 
 val num_rules : t -> int
-(** [get_num_rules j] is the number of rules that [j] has *)
+(** [get_num_rules t] is the number of rules that [t] has *)
 
 val random_rule : rule list -> rule
-(** [random_rule t] is the rule randomly picked from the list of rules
-    [t.rules]. Raises [NoRules] if [t] has no rules.*)
+(** [random_rule lst] is the rule randomly picked from the list of rules [lst].
+    Raises [NoRules] if [lst] has no rules.*)
